3601a81be9d7417dfcff0f35212cd87c558ea5ae
hhammer1001 <89806911+hhammer1001@users.noreply.github.com>
2024-11-11 13:45:26 -0600

    try again

--- Game.py
+++ Game.py
@@ -6,6 +6,9 @@ class Game():
         self.centerK = 5*round(self.startS/5)
         self.Ks = [self.centerK + 5*i for i in range(-numKs//2+1, numKs//2+1)]
         self.moves =  [0.01*x if x <6 else -0.01*(x-5) for x in range(1, 11)]
+        for k in self.Ks:           #calls
+            intrin = self.startS - k
+            
 
     def __repr__(self):
         return str(self.startS)+", "+str(self.Ks)
@@ -13,8 +16,13 @@ class Game():
     def movement(self):
         return choice(self.moves)
 
+
+
+
+
 def play(numKs, width):
     pass
+
 
 
 
