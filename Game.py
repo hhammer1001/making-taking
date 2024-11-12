61fda09e44554864d24ea81a7cead7cd501767ec
hhammer1001 <89806911+hhammer1001@users.noreply.github.com>
2024-11-12 09:49:53 -0600

    implemented black scholes fully

--- Game.py
+++ Game.py
@@ -1,48 +1,59 @@
 from random import *
 import math
-
+import numpy as np
+from scipy.stats import norm
 # Black-Scholes:
 
 
 
 class Game():
     def __init__(self, numKs = 5, width = [50, 100], moveType = "uniform"):
-        self.startS = (randint(width[0], width[1])+random())//.01/100
+        # self.startS = (randint(width[0], width[1])+random())//.01/100
+        self.startS = 75
         self.centerK = 5*round(self.startS/5)
         self.Ks = [self.centerK + 5*i for i in range(-numKs//2+1, numKs//2+1)]
         if moveType == "uniform":
             self.moves =  [0.01*x if x <6 else -0.01*(x-5) for x in range(1, 11)]
         else:
             self.moves = [0.01, -0.01]
-        self.vol = 40
+        self.vol = .40
         self.dte = 28/365
         self.intRate = 0.05
-        for k in self.Ks:           #calls
-            intrin = self.startS - k
+        self.calls = [blackScholes("c", self.startS, strike, self.intRate, self.dte, self.vol) for strike in self.Ks]
+        self.puts = [blackScholes("p", self.startS, strike, self.intRate, self.dte, self.vol) for strike in self.Ks]
+
+            
             
 
 
     def __repr__(self):
-        return str(self.startS)+", "+str(self.Ks)
+        return str(self.startS)+", "+str(self.Ks)+"\n"+str(self.calls)+"\n"+str(self.puts)
 
     def movement(self):
         return choice(self.moves)
 
 
-def blackScholes(s, k, r, t, v):
+def blackScholes(optType, s, k, r, t, v):
     d1 = (math.log(s/k) + (r+(v**2)/2)*t)/(v*(t**0.5))
     d2 = d1 - v*(t**0.5)
-    
+    if optType == "c":
+        return s*norm.cdf(d1)-k*math.exp(-r*t)*norm.cdf(d2)
+    else:
+        return -s*norm.cdf(-d1)+k*math.exp(-r*t)*norm.cdf(-d2)
+
+
+# def CND(x):
+#   if (x < 0):
+#     return 1 - CND(-x);
+#   else:
+#     k = 1 / (1 + .2316419 * x);
+#     return 1 - math.exp(-x * x / 2) / math.sqrt(2 * math.pi) * k * (.31938153 + k * (-.356563782 + k * (1.781477937 + k * (-1.821255978 + k * 1.330274429))));
 
 def play(numKs, width):
     pass
 
 
 
-
-
-
-
 g1 = Game()
 # print(g1.moves)
-print(blackScholes(g1.startS, g1.centerK, g1.intRate, g1.dte, g1.vol))
+print(blackScholes("c", g1.startS, g1.centerK, g1.intRate, g1.dte, g1.vol))
