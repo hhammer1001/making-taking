90758e39b45a97be5c1f6eee2932cc8458a74b7b
Henry <hhammer@hmc.edu>
2024-12-11 17:26:13 +0000

    started (tk)inting

--- Game.py
+++ Game.py
@@ -2,6 +2,8 @@ from random import *
 import math
 import numpy as np
 from scipy.stats import norm
+import tkinter as tk
+from colored import Style
 # Black-Scholes:
 
 
@@ -29,6 +31,7 @@ class Game():
         self.PNL = 0
         self.delta = 0
         self.optionTypes = {"c":"call", "p":"put"}
+        self.underline = Style.underline_color('Black')
 
 
 
@@ -58,7 +61,32 @@ class Game():
         board += "―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\n"
         print(board)
         return
-        
+    
+    def tkBoard(self, wind):
+        wind.rowconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8], minsize = 50, weight=1)
+        wind.columnconfigure([0,1,2,3,4,5,6,7,8], minsize = 50, weight=1)
+        lbl_value = tk.Label(master=wind, text=f"""Starting Price: {self.startS}     Vol: {self.vol*100}
+        Current Price: {self.curS}     PNL: {self.PNL}     Delta: {self.delta}""")
+        lbl_value.grid(row=0, column = 0)
+        cols = ["Delta", 'Bid', 'Call', 'Offer', 'Strike', 'Bid', 'Put', 'Offer']
+        for i in range(8):
+            frame = tk.Frame(master=wind, relief=tk.SUNKEN, borderwidth=1)
+            frame.grid(row=0, column=i+1, padx=5, pady=5)
+            label = tk.Label(master=frame, text=cols[i])
+            label.pack(padx=5, pady=5)
+        opts = []
+        r = 0
+        for strike in self.Ks:
+            r += 1
+            opts = [round(blackScholes("d", self.startS, strike, self.intRate, self.dte, self.vol),2), 0, round(blackScholes("c", self.startS, strike, self.intRate, self.dte, self.vol), 2),0, strike, 0, round(blackScholes("p", self.startS, strike, self.intRate, self.dte, self.vol),2), 0]
+            rowVals = [str(x)+"0" if (x == round(x,1) and x != round(x, 0)) else str(x) for x in opts]
+            for i in range(8):
+                frame = tk.Frame(master=wind, borderwidth=1)
+                frame.grid(row=r, column=i+1, padx=5, pady=5)
+                label = tk.Label(master=frame, text=rowVals[i])
+                label.pack(padx=5, pady=5)
+
+
     def generateOrder(self, optType, orderType, strike):
         theo = round(blackScholes(optType, self.curS, strike, self.intRate, self.dte, self.vol),2)
         alpha = gauss(0,0.1)
@@ -68,7 +96,7 @@ class Game():
             return f"{price} bid for {q} {strike} {self.optionTypes[optType]}s"
         if orderType == "o":
             return f"offer {q} {strike} {self.optionTypes[optType]}s for {price}"
-
+        
 
     def tick(self):
         temp = self.randomMove()
@@ -108,7 +136,8 @@ def play(numKs, width):
 
 
 g1 = Game()
-for i in range(10):
-    g1.tick()
-
-
+# for i in range(10):
+#     g1.tick()
+window = tk.Tk()
+g1.tkBoard(window)
+window.mainloop()
